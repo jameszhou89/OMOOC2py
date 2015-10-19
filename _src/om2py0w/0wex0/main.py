70953319d4a37b1f31ac9e755b3f29d1b8692a07
jameszhou89 <zhouhuiming1989@gmail.com>
2015-10-19 22:30:32 +0800

    if
    sign of break

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -1,7 +1,8 @@
 total=0
 numbers=[]
 number= raw_input("what is the number in you mind now?")
+if number=="done" ： break
 numbers.append(number)
-for i in numbers[]:
+for i in numbers:
 	total=total+i
 print total
