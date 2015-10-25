3c9ca0dc6155d5ad1bce222ddd88ce35b4c0c8ad
jameszhou89 <zhouhuiming1989@gmail.com>
2015-10-25 16:40:22 +0800

    2nd edition
    加入了日记回读，以及持续性输入

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,15 +1,50 @@
-import sys 
+# -*- coding: utf-8 -*-
+
+import sys
 import time
 
+line2=""
+line1=""
 
 target = open("diary.txt", 'a+')
 
-print "Now I'm going to ask you for 1 lines."
+def writedairy():
+	target = open("diary.txt", 'a+')
+	print "what do u want to say TODAY?"
+	line1 = raw_input("line 1: ")
 
-line1 = raw_input("line 1: ")
+	print "I'm going to write these to the file diary.txt."
 
-print "I'm going to write these to the file."
+	target.write(line1+"  ")
+	target.write(time.strftime("%Y/%m/%d%H:%M:%S"))
+	target.write("\n")
+	
+	#print "here is what you have written"
+	#target = open("diary.txt", 'a+')
+	#print target.read()
 
-target.write(line1+"  ")
-target.write(time.strftime("%Y/%m/%d%H:%M:%S"))
-target.write("\n")
+#def askforcontinue():
+	#print "do u want to continue?if you want to continue print Y, want to quit print N"
+	#line2 = raw_input("Y/N ")
+
+writedairy()
+#askforcontinue()
+print "here is what you have written"
+target = open("diary.txt")
+print target.read()
+
+while True:
+	line2 = raw_input("do u want to continue?if you want to continue print Y, want to quit print N ")
+	if line2=="Y":
+		writedairy()
+		print "here is what you have written"
+		target = open("diary.txt")
+		print target.read()
+	elif line2=="N":
+		target.close()
+		break
+		print "And finally, we close it."
+
+
+print "And finally, we close it."
+#target.close()
