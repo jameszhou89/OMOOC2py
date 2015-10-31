a789d1a43335f8ed59c07e9851489fde3d03ef7e
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-01 00:02:42 +0800

    final version
    参考自wzzlj

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -1,44 +1,74 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 from Tkinter import *
+import tkMessageBox
 
-class App:
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-    def __init__(self, master):
-
-        frame = Frame(master)
-        frame.pack()
-
-        self.button = Button(
-            frame, text="QUIT", fg="red", command=frame.quit
-            )
-        self.button.pack(side=LEFT)
-        
-        e = Entry(root,width=50)
-        e.pack()
-        e.focus_set()
-
-        def callback():
-            print e.get()
-
-        b = Button(root, text="get", width=10, command=callback)
-        b.pack()
-      
+import time
 
 root = Tk()
+root.geometry('600x30+400+400')
 
-app = App(root)
+class Application(Frame):
 
-#e = Entry(root,width=50)
-#e.pack()
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-#e.focus_set()
+# 写入日记
+    def write(self):
+        target = open('dairy.txt','a')
+        line = self.Input.get()
+        self.Input.delete(0,END) # 保存后清空文字框
+        target.write('\n' + line)
+        target.write(time.strftime("%Y/%m/%d%H:%M:%S"))
+        target.close()
 
-#def callback():
-    #print e.get()
+    
+# 读取日记
+    def read(self):
+        tkMessageBox.showinfo('读日记',open('dairy.txt','r').read())
 
-#b = Button(root, text="get", width=10, command=callback)
-#b.pack()
+# 清除日记
+    def clear(self):
+        target = open('dairy.txt','w')
+        target.close()
 
-root.mainloop()
-root.destroy() # optional; see description below
+
+
+# 创建组件
+    def createWidgets(self):
+
+        self.readbutton = Button(self, text='读日记',command=self.read)
+        self.readbutton.pack(side=RIGHT)
+
+        self.label1 = Label(self, text='输入文字:').pack(side=LEFT)
+        self.Input = Entry(self, width=43)
+        self.Input.pack(side=LEFT)
+
+        self.saveButton = Button(self)
+        self.saveButton['text'] = '保存'
+        self.saveButton['command'] = self.write
+        self.saveButton.pack(side=LEFT)
+
+        menubar = Menu(self)
+        self.filemenu = Menu(menubar, tearoff=0)
+        self.filemenu2 = Menu(menubar, tearoff=1)
+        menubar.add_cascade(label='File', menu=self.filemenu)
+        menubar.add_cascade(label='Author:xmuzlj@gmail.com', menu=self.filemenu2)
+        self.filemenu.add_command(label='初始化',command=self.clear)
+        self.filemenu.add_command(label='退出',command=self.quit)
+        self.master.config(menu = menubar)
+
+def main():
+    app = Application(master = root)
+    app.master.title('简单日记本')
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
