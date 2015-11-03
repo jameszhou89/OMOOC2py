8c15fe88e19316dab7a42caed654432e07376181
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-04 01:24:48 +0800

    最简单的UDP
    2nd edition ，修正了一个Bug,让两个client能够跟一个server同时对话

--- _src/om2py3w/3wex0/server0.py
+++ _src/om2py3w/3wex0/server0.py
@@ -6,12 +6,12 @@ HOST = 'localhost'                 # Symbolic name meaning all available interfa
 PORT = 10000            # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen(10)
-print 'socket now listening'
-conn, addr = s.accept()
+
 
 while 1:
-	
+	s.listen(10)
+	print 'socket now listening'
+	conn, addr = s.accept()
 	print 'Connected by', addr
 	data = conn.recv(1024)
     #if not data: break
