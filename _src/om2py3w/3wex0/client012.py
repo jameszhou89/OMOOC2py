e212dccd459f2b1a9af4b1c07c92f3f4d0628ff1
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-04 01:42:54 +0800

    追加笔记系统
    1st edition

--- _src/om2py3w/3wex0/client012.py
+++ _src/om2py3w/3wex0/client012.py
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
 # Echo client program
 import socket
 import sys
+
+
+line1=""
 
 HOST = 'localhost'    # The remote host
 PORT = 10000            # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+
+line1=raw_input("write:")
+s.sendall(line1)
+
+
 data = s.recv(1024)
 s.close()
 print 'Received', repr(data)
