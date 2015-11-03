4ec76b6d986e20488bc35f8eb3fab75e401649ad
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-04 02:13:16 +0800

    持续输入
    2nd edition

--- _src/om2py3w/3wex0/client012.py
+++ _src/om2py3w/3wex0/client012.py
@@ -8,8 +8,8 @@ line1=""
 
 def writedairy():
 	print "what do u want to say TODAY?"
-	line2 = raw_input("line 1: ")
-	return line2
+	line1 = raw_input("line 1: ")
+	return line1
 
 
 
@@ -22,12 +22,13 @@ s.connect((HOST, PORT))
 while True:
 	line2 = raw_input("do u want to continue?if you want to continue print Y, want to quit print N ")
 	if line2=="Y":
-		writedairy()
-		s.sendall(line2)
-		data = s.recv(1024)
+		line1=writedairy()
+		s.sendall(line1)
+		data = s.recv(1024) 
+		print 'Received', repr(data)
+		s.close()
 	elif line2=="N":
 		break
 		print "And finally, we close it."
 
 s.close()
-print 'Received', repr(data)
