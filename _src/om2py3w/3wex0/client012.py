49154f1ff7fa704b11346936928017e40a925f7f
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-04 02:11:29 +0800

    持续输入
    1st edition

--- _src/om2py3w/3wex0/client012.py
+++ _src/om2py3w/3wex0/client012.py
@@ -3,18 +3,31 @@
 import socket
 import sys
 
-
 line1=""
+
+
+def writedairy():
+	print "what do u want to say TODAY?"
+	line2 = raw_input("line 1: ")
+	return line2
+
+
 
 HOST = 'localhost'    # The remote host
 PORT = 10000            # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-line1=raw_input("write:")
-s.sendall(line1)
 
+while True:
+	line2 = raw_input("do u want to continue?if you want to continue print Y, want to quit print N ")
+	if line2=="Y":
+		writedairy()
+		s.sendall(line2)
+		data = s.recv(1024)
+	elif line2=="N":
+		break
+		print "And finally, we close it."
 
-data = s.recv(1024)
 s.close()
 print 'Received', repr(data)
