8ee044ac3d8dce91eef42f5602c462e1fe5157c3
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-08 22:58:38 +0800

    final version

--- _src/om2py3w/3wex0/client21.py
+++ _src/om2py3w/3wex0/client21.py
@@ -3,29 +3,46 @@
 import socket
 import sys
 
+#建立协议，当client发送信息为keyword时，返回所有日记历史信息
+KEYWORD = 'P'
 
+def main():
 # Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 10000)
+    server_address = ('localhost', 10000)
 #message = 'This is the message.  It will be repeated.'
 
-message=''
-
-while True:
-  b_message = raw_input('>>> ')
-  if b_message=="Y":
-    a_message = raw_input('>>> ')
-    message += a_message + '\n'
-
-    print >>sys.stderr, 'sending "%s"' % message
-    sent = sock.sendto(message,server_address)
-    # Re sceive response
-    print >>sys.stderr, 'waiting to receive'
+    sock.sendto(KEYWORD,server_address)
     data, server = sock.recvfrom(4096)
-    print >>sys.stderr, 'received "%s"' % data
-  elif b_message=="N":
-    break
+    print data
+
+    message=''
+
+    while True:
+        b_message = raw_input('"if you want to continue print Y, want to quit print N " ')
+        if b_message=="Y":
+            a_message = raw_input('>>> ')
+            message = a_message
+
+            print >>sys.stderr, 'sending "%s"' % message
+            sent = sock.sendto(message,server_address)
+
+            if message=="q":
+              break
+
+    #如何保存数据？
 
 
-sock.close()
+    # Receive response
+            print >>sys.stderr, 'waiting to receive'
+            data, server = sock.recvfrom(4096)
+            print >>sys.stderr, 'received "%s"' % data
+
+        elif b_message=="N":
+            break
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
--- _src/om2py3w/3wex0/server1.py
+++ _src/om2py3w/3wex0/server1.py
@@ -1,20 +1,57 @@
+# -*- coding: utf-8 -*-
 import socket
 import sys
+import jamesdiary
 
+# 建立协议，当client发送信息为keyword时，返回所有日记历史信息
+KEYWORD = 'P'
+
+def response1(sock, data, address):
+    """定义反应行为，如果接收到信息为KEYWORD，发回日记历史，否则则将信息记入日记"""
+    if data == 'P':
+        history_message = jamesdiary.readdiary()
+        sent = sock.sendto(history_message, address)
+        print >>sys.stderr, 'sent %s back to %s' % (sent, address)
+ #   elif data=="q":
+ #       sock.close()    
+    else: 
+        sent = sock.sendto(data, address)
+        
+        print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+        print >>sys.stderr, data
+
+        jamesdiary.writediary(data)
+    
+
+
+
+def main():
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+    server_address = ('localhost', 10000)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
 
-while True:
-  print >>sys.stderr, '\nwaiting to receive message'
-  data, address = sock.recvfrom(4096)
-    
-  print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-  print >>sys.stderr, data
-  if data:
-    sent = sock.sendto(data, address)
-    print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+    while True:
+        print >>sys.stderr, '\nwaiting to receive message'
+        data, address = sock.recvfrom(4096)
+        #print "py debug",len(data)
+        if data == 'q':
+            break
+        response1(sock, data, address)
+
+        #print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+        #print >>sys.stderr, data
+    #if data:
+        #sent = sock.sendto(data, address)
+        #print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
