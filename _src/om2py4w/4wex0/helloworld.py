6c68371aeb71bbc395ec0cdd28ccef3f60dc99e6
jameszhou89 <zhouhuiming1989@gmail.com>
2015-11-15 01:08:42 +0800

    动态输入

--- _src/om2py4w/4wex0/helloworld.py
+++ _src/om2py4w/4wex0/helloworld.py
@@ -2,9 +2,22 @@
 
 from bottle import route, run, template
 
+data=""
+
 @route('/hello')
-def hello():
-    return "Hello World!中"
+def hello(data):
+    return data
+
+
+def main();
+    while True:
+        line2 = raw_input("if you want to continue print Y, want to quit print N ")
+        if line2=="Y":
+            data=raw_input("-->")
+    	    run(host='localhost', port=8080,debug=True)
+        elif line2=="N":
+    	    break
+
 
 if __name__ == "__main__":
-    run(host='localhost', port=8080,debug=True)
+    main（）
